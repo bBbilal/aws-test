58e81f0ce92954ec54887e019aae3815495305b7
Bilal BOUTAYA <bill.bilal1984@gmail.com>
2016-12-28 22:27:10 +0100

    initial commit

---
+++ src/main/java/org/test/TestawsApplication.java
@@ -0,0 +1,12 @@
+package org.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestawsApplication.class, args);
+	}
+}
